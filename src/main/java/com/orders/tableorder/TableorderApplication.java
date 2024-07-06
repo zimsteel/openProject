91ffe301612f87142870bda21e7a034e41aceede
steel koh <zimvabu@naver.com>
2024-07-07 00:17:00 +0900

    spring boot base init commit

---
+++ src/main/java/com/orders/tableorder/TableorderApplication.java
@@ -0,0 +1,13 @@
+package com.orders.tableorder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TableorderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TableorderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/orders/tableorder/TableorderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.orders.tableorder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TableorderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
